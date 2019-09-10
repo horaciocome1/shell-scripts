5795b90a322bb4abf94101ebe21cfdfb386ef032
Horácio Comé <horaciojr001@gmail.com>
2019-09-10 18:56:22 +0200

    Update cleanSwap.sh
    fixed error due to unexpected line break in the middle of an echo command

--- cleanSwap.sh
+++ cleanSwap.sh
@@ -15,11 +15,11 @@ else
     echo "Uso de memória RAM antes de fazer a limpeza:" $MEM1%
     echo "**************************************************************************"
     echo " "
-    echo "Limpando à memória RAM..."
+    echo "Limpando a memória RAM..."
     sleep 3
     sync
     echo 2 > /proc/sys/vm/drop_caches
-    echo "Limpando à memória Swap..."
+    echo "Limpando a memória Swap..."
     sleep 3
     swapoff -a
     swapon /dev/sda2
@@ -27,8 +27,9 @@ else
     MEM2=`free|awk '/Mem:/ {print int(100*$3/$2)}'`
     free -m
     echo "**************************************************************************"
-    echo "Uso de memória RAM após a limpeza:" $MEM2% "Economia de: " `expr $MEM1 -      $MEM2`"%"
+    echo "Uso de memória RAM após a limpeza:" $MEM2% "Economia de: " `expr $MEM1 
+-      $MEM2`"%"
     echo "**************************************************************************"
     sleep 1
     exit
-fi
+fi 
